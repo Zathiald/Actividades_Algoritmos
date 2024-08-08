0e22777e41d4ee1843d8054c8bf61078203f1e56
Zathiald <A01705403@tec.mx>
2024-08-08 14:42:50 -0600

    Ya esta el problema 5 a huevo ptm

--- Act1.1/Problema5.py
+++ Act1.1/Problema5.py
@@ -1,5 +1,17 @@
-def calcular_serie():
-    calculoFinal = 0
-    numeroDiv=1
-    for i in range(0, 15):
-        break;
+def calcular_sumatoria(n):
+    termino = 0
+    exponente = 0
+    suma = 0
+    for i in range(1, n + 1):
+        if i % 2 == 0:
+            exponente = 2
+        else:
+            exponente = i
+        termino = 1 / (i ** exponente)
+        suma += termino
+    return suma
+
+# Calcular los primeros 15 términos de la sumatoria
+n = 15
+resultado = calcular_sumatoria(n)
+print(f"La sumatoria de los primeros {n} términos es: {resultado:.6f}")
