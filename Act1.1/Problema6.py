d622df5803e6c0d187d475c154436bc4362c8f7d
Ángel Francisco García Guzmán <angelgarciag14@gmail.com>
2024-08-08 14:20:05 -0600

    Problema 6 "Factorial"

---
+++ Act1.1/Problema6.py
@@ -0,0 +1,17 @@
+def calcular_factorial(numero):
+    if numero < 0:
+        return "El factorial no está definido para números negativos."
+    elif numero == 0 or numero == 1:
+        return 1
+    else:
+        factorial = 1
+        for i in range(2, numero + 1):
+            factorial *= i
+        return factorial
+
+# Solicitar al usuario que ingrese un número
+numero = int(input("Ingresa un número: "))
+
+# Calcular y mostrar el factorial
+resultado = calcular_factorial(numero)
+print(f"El factorial de {numero} es: {resultado}")
