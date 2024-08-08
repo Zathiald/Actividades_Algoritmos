a221fd5934cd459a3cf2b52ce03e6924d93d14ef
Ángel Francisco García Guzmán <angelgarciag14@gmail.com>
2024-08-08 14:27:41 -0600

    Problema 3 "Random"

---
+++ Act1.1/Problema3.py
@@ -0,0 +1,15 @@
+import random
+
+def generar_numeros_aleatorios(cantidad):
+    numeros = []
+    for _ in range(cantidad):
+        numero = random.randint(-100, 100)  # Genera un número aleatorio entre -100 y 100
+        numeros.append(numero)
+    return numeros
+
+# Solicitar al usuario que ingrese un número
+cantidad = int(input("Ingresa la cantidad de números aleatorios que deseas: "))
+
+# Generar y mostrar los números aleatorios
+numeros_aleatorios = generar_numeros_aleatorios(cantidad)
+print(f"Los {cantidad} números aleatorios son: {numeros_aleatorios}")
